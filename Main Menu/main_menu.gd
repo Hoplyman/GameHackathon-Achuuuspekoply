258efc33c1620b962d8629da97b546358aa18bd1
Francis Gabriel Austria <135101084+SpekyLord@users.noreply.github.com>
2025-08-21 20:55:56 +0800

    Update main menu scene transitions and signals
    Changed main menu button signal connections to use dedicated methods for Start and Versus buttons, both now loading 'res://scenes/Board.tscn'. Updated main scene UID in project settings and level scene UIDs for consistency.

--- Main Menu/main_menu.gd
+++ Main Menu/main_menu.gd
@@ -3,12 +3,17 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("Game Scene chuchu")
+	get_tree().change_scene_to_file("res://scenes/Board.tscn")
 
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("Options")
 
 
+
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_versus_pressed():
+	get_tree().change_scene_to_file("res://scenes/Board.tscn")
