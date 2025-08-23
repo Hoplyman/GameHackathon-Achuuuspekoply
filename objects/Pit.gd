5a0b78e2b99f15fbcd753f905951d6abb5b0007e
Hoplyman <jorgemaverick.acidre@iloilonhs.edu.ph>
2025-08-23 14:49:35 +0800

    Refactor pit shell logic and add campaign mode support
    Refactored Pit.gd to support both PvP and Campaign modes by detecting the active mode and interacting with the appropriate node. Added campaign.gd script for shell management in campaign mode and updated scene files to remove unused groups and assign new scripts. Deleted Board.tscn and cleaned up main house group assignments.

--- objects/Pit.gd
+++ objects/Pit.gd
@@ -5,7 +5,6 @@ extends Node2D
 
 var shells: int = 0
 var fshells: int = 0
-var gameplay  # assigned externally by the manager
 
 func _ready():
 	update_label()
@@ -34,17 +33,34 @@ func add_shells(amount: int):
 	spawn_shells()
 
 func spawn_shells():
+	var gamemode: String = ""
+	var pvp = get_tree().root.get_node("Gameplay")
+	var campaign = get_tree().root.get_node("Campaign")
 	var pits = get_tree().get_nodes_in_group("pits")
 	var pit_index = pits.find(self)
-
-	if pit_index != -1 and gameplay:
+	if campaign:
+		print("Campaign found!")
+		gamemode = "Campaign"
+	elif  pvp:
+		print("Gameplay found!")
+		gamemode = ("Pvp")
+	else:
+		gamemode = ""
+	if pit_index != -1 and gamemode == "Campaign":
 		var pit_node = pits[pit_index]
 		var pitx = pit_node.position.x
 		var pity = pit_node.position.y
-		gameplay.set_shells(shells, pitx, pity)
+		campaign.set_shells(shells, pitx, pity)
+		print("Pit found in group")
+	elif pit_index != -1 and gamemode == "Pvp":
+		var pit_node = pits[pit_index]
+		var pitx = pit_node.position.x
+		var pity = pit_node.position.y
+		pvp.set_shells(shells, pitx, pity)
 		print("Pit found in group")
 	else:
-		print("Pit not found in group or gameplay not assigned!")
+		print("Pit not found in group")
+		return 0
 
 func update_label():
 	var shell_count = fshells
@@ -54,16 +70,33 @@ func update_label():
 		print("Warning: ShellLabel not found in Pit")
 		
 func count_shells_in_area() -> int:
+	var gamemode: String = ""
+	var pvp = get_tree().root.get_node("Gameplay")
+	var campaign = get_tree().root.get_node("Campaign")
 	fshells = 0
-	if not gameplay:
-		print("Gameplay not assigned in Pit!")
+	if campaign:
+		print("Campaign found!")
+		gamemode = "Campaign"
+	elif  pvp:
+		print("Gameplay found!")
+		gamemode = ("Pvp")
+	else:
+		gamemode = ""
+	if gamemode == "Campaign":
+		for child in campaign.get_children():
+			if child.name.begins_with("Shell"):  # Or use `is Shell` if you use a script class
+				if shell_area and shell_area.overlaps_body(child):
+					fshells += 1
+		return fshells
+	elif gamemode == "Pvp":
+		for child in pvp.get_children():
+			if child.name.begins_with("Shell"):  # Or use `is Shell` if you use a script class
+				if shell_area and shell_area.overlaps_body(child):
+					fshells += 1
+		return fshells
+	else:
 		return 0
-
-	for child in gameplay.get_children():
-		if child.name.begins_with("Shell"):  # Or use `is Shell` if you use a script class
-			if shell_area and shell_area.overlaps_body(child):
-				fshells += 1
-	return fshells
+	
 
 func _on_gravity_area_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
